5d721446c9949dc6c9f100aaf2db3b508b71804e
Silvio <silviotassini@gmail.com>
2023-07-21 12:33:13 -0300

    corrected and generalized tests

--- test1.py
+++ test1.py
@@ -1,8 +1,9 @@
 import unittest
+from parameterized import parameterized
 from bfs import bfs
 from dfs import dfs
 
-graph = {
+graph0 = {
     '0': ['2', '3', '4'],
     '1': ['2', '4'],
     '2': ['4'],
@@ -11,19 +12,25 @@ graph = {
     '5': ['1']
 }
 
-class TestGraphTraversal(unittest.TestCase):
-    def test_bfs(self):
-        expected_output = ['0', '2', '3', '4', '5', '1']   # The expected BFS traversal order
+bfs_expected0 = ['0', '2', '3', '4', '5', '1'] 
+dfs_expected0 = ['0', '4', '5', '1', '2', '3']
 
+class TestGraphTraversal(unittest.TestCase):
+
+    @parameterized.expand([
+        (graph0, bfs_expected0),     
+    ])
+    def test_bfs(self, graph, expected_output):
         # Call the BFS function and store the output in bfs_result
         bfs_result = bfs(graph, '0')
-        
+        print(bfs_result)
         # Assert that the bfs_result is equal to the expected_output
         self.assertEqual(bfs_result, expected_output)
 
-    def test_dfs(self):
-        expected_output = ['0', '2', '4', '5', '1', '3']   # The expected DFS traversal order
-
+    @parameterized.expand([
+        (graph0, dfs_expected0),     
+    ])
+    def test_dfs(self, graph, expected_output):
         # Call the DFS function and store the output in dfs_result
         dfs_result = dfs(graph, '0')
         
